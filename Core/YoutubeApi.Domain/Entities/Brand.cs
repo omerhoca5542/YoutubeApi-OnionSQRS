d99c5dcea814256c020096d1f9b5bdd96a9de57b
omerhoca5542 <moment5@hotmail.com>
2024-01-14 19:10:31 +0300

    Temel entityler oluşturuldu.

---
+++ Core/YoutubeApi.Domain/Entities/Brand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Brand:EntitityBase//brand marka demek sitedeki marka isimleri için oluşturduk bu alanı
+    {
+        public Brand()
+        {
+                
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+
+        public required string Name { get; set; }
+    }
+}
---
+++ Core/YoutubeApi.Domain/Entities/Detail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public  class Detail:EntitityBase// detail entitysi satılan her ürünün kategorisine göre özllikleri farklı olacağından biz burda kategoriye göre özellik tanımlıcaz.örneğin bilgisayar için ram ekran kartı özellikse masa için en boy malzeme kısmı özelliktir
+    {
+        public Detail()
+        {
+                
+        }
+        public Detail(string title,string description,int categoryId)
+        {
+            Title = title;
+            Description = description;
+            CategoryId = categoryId;
+        }
+        public required string Title { get; set; }// ttitle ise  malın kategorisine ait özellik ismi olacak
+        public required string Description { get; set; }// tanımlama kısmı 
+        public required int CategoryId { get; set; }// mürünlerin kategorisini alıcaz ki ona göre özellikler gelsin 
+        public Category Category { get; set; }//Bir detayın sadece bir tane kategorisi olacağı için bire bir ilişkilendirme yaptık
+    }
+}
